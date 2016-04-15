7610dc9dde54674a1b3607d2b616495b119983c1
zhoujianguo <zhoujianguowei@gmail.com>
2016-04-15 11:47:01 +0800

    initial oobServer project

---
+++ oobServer/src/jianguo/servletPac/RegisterServlet.java
@@ -0,0 +1,33 @@
+package jianguo.servletPac;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * Created by Administrator on 2016/4/15.
+ */
+public class RegisterServlet extends HttpServlet
+{
+
+    @Override protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+//        super.doGet(req, resp);
+        doPost(req, resp);
+    }
+    @Override protected void doPost(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException
+    {
+//        super.doPost(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Hello World</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println("<BIG>Hello World</BIG>");
+        out.println("</BODY></HTML>");
+    }
+}
