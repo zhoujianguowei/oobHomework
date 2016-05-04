5b65b5c7f6764526b8d44a70fce4cb9898c146ee
zhoujianguo <zhoujianguowei@gmail.com>
2016-05-04 13:50:40 +0800

    remove previous oobServer due to updated Intellij ,and recreate adriftbook web project instead

--- oobClient/OobHomeWorkClient/UploadFileModule/src/main/java/com/liucanwen/asyncuploadfiletest/MainActivity.java
+++ oobClient/OobHomeWorkClient/UploadFileModule/src/main/java/com/liucanwen/asyncuploadfiletest/MainActivity.java
@@ -2,9 +2,11 @@ package com.liucanwen.asyncuploadfiletest;
 import android.app.Activity;
 import android.os.Bundle;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.klicen.constant.Constant;
 import com.loopj.android.http.AsyncHttpClient;
 import com.loopj.android.http.AsyncHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
@@ -13,6 +15,7 @@ import org.apache.http.Header;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.UnsupportedEncodingException;
 public class MainActivity extends Activity
 {
 
@@ -28,10 +31,10 @@ public class MainActivity extends Activity
     public void uploadFile()
     {
         //服务器端地址
-        String url = "http://192.168.0.108:8080/UploadFileServer/upload";
+        String url = Constant.CONSTANT_IP + "upload";
         //手机端要上传的文件，首先要保存你手机上存在该文件
         String filePath = Environment.getExternalStorageDirectory()
-                + "/1/power.apk";
+                + "/Android/上传图片.jpg";
         AsyncHttpClient httpClient = new AsyncHttpClient();
         RequestParams param = new RequestParams();
         try
@@ -49,14 +52,28 @@ public class MainActivity extends Activity
                 @Override
                 public void onSuccess(int i, Header[] headers, byte[] bytes)
                 {
+                    Toast.makeText(MainActivity.this, "上传成功", Toast.LENGTH_LONG)
+                            .show();
                 }
                 @Override
                 public void onFailure(int i, Header[] headers, byte[] bytes,
                                       Throwable throwable)
                 {
+                    try
+                    {
+                        Toast.makeText(MainActivity.this,
+                                "上传失败",
+                                Toast.LENGTH_LONG)
+                                .show();
+                        if (bytes != null)
+                            Log.e("failure",
+                                    new String(bytes, Constant.DEFAULT_CODE));
+                    }
+                    catch (UnsupportedEncodingException e)
+                    {
+                        e.printStackTrace();
+                    }
                 }
-
-
             });
         }
         catch (FileNotFoundException e)
